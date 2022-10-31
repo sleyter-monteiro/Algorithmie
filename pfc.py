0b592d59f18c15a38740b3a5ba0d164b5ada66dd
Sleyter MONTEIRO <87656234+sleyter92k@users.noreply.github.com>
2022-10-31 12:35:32 +0100

    Introduction

---
+++ pfc.py
@@ -0,0 +1,28 @@
+# test
+def choice_pfc():
+    ***
+    Arg :
+        - player (int)
+        - posssible choice (array)
+    Return :
+        - choice : str    
+    ***
+
+str_test = 'coucou'
+print(str_test[6])
+
+array_test = [1, 2, 3, 4, 5]
+array_str = ['a', 'b', 'c', 'd', 'e']
+array_float = [1.1 2.2 3.3 4.4 5,5]
+
+int_test = 1
+str_int_test = str(int_test)
+
+print('for loop')
+for i in array_test:
+    print(i)
+
+print('while loop')
+1 = 0
+while i < len(array_test):
+    print(array_test[])
