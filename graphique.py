085ed462f11f4ec1ea7927089dd563913748acef
Myriam Faridi <117079568+thegentleslayer@users.noreply.github.com>
2022-11-07 11:50:03 +0100

    mod

--- graphique.py
+++ graphique.py
@@ -1,54 +1,11 @@
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-
-
-
-game_images = [rock, paper, scissors]
-
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[user_choice])
-
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])
-
-
-
-
-
-
-
-
-
 import random
 nom = input ("Veuillez saisir votre nom : ")
 user_victoires = 0
 pc_victoires = 0
 nuls = 0
+manches_user_victoires = 0
+manche_pc_victoires = 0
+manche_total = 3
 
 while True : 
     print(nom, " : " , user_victoires, "égalités : ", nuls, " PC: ", pc_victoires)
@@ -66,6 +23,7 @@ while True :
     else: 
         print("CISEAUX contre ...", end= " ")
 
+
     
     randomNombre = random.randint(1,3)
     if randomNombre == 1 :
@@ -81,21 +39,28 @@ while True :
     if user_choice == pc_choice :
         print("Match nul!")
         nuls = nuls + 1
+        manche_total += 0
     elif user_choice == "p" and pc_choice == "c" :
         print("Vous avez gagné(e)!")
         user_victoires = user_victoires + 1
+       manches_user_victoires += 1
     elif user_choice == "f" and pc_choice == "p" :
         print("Vous avez gagné(e)!")
         user_victoires = user_victoires + 1
+        manches_user_victoires += 1
     elif user_choice == "c" and pc_choice == "f" :
         print("Vous avez gagné(e)!")
         user_victoires = user_victoires + 1
+        manches_user_victoires += 1
     elif user_choice == "p" and pc_choice == "f" :
         print("Vous avez perdu(e)!")
         pc_victoires = pc_victoires + 1
+        manche_pc_victoires += 1
     elif user_choice == "f" and pc_choice == "c" :
         print("Vous avez perdu(e)!")
         pc_victoires = pc_victoires + 1
+        manche_pc_victoires += 1
     elif user_choice == "c" and pc_choice == "p" :
         print("Vous avez perdu(e)!")
         pc_victoires = pc_victoires + 1
+        manche_pc_victoires += 1
