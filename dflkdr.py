292b6f148aef4ab78243ca0f756cee33148f27db
Sleyter MONTEIRO <87656234+sleyter92k@users.noreply.github.com>
2022-11-10 01:26:29 +0100

    Réorganisation du Repo
    Dossier PFC Morpion et Pseudo Code

--- dflkdr.py
+++
@@ -1,3 +0,0 @@
-import random
-liste_pfc = ["pierre","papier","ciseaux"]
-print(random.choice(liste_pfc))
--- idea_keep.py
+++
@@ -1,6 +0,0 @@
-a=input("choisi la colone")
-b=input("Choisi la ligne")
-distance = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(distance[0])
-print(distance[1])
-print(distance[2])
